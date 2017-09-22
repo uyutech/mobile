7267667f13f97167027ebbd26428315dfa631cff
army8735 <army8735@qq.com>
2017-09-22 14:58:16 +0800

    comment

--- src/author/AuthorComment.jsx
+++ src/author/AuthorComment.jsx
@@ -195,6 +195,7 @@ class AuthorComment extends migi.Component {
         ParentID,
         RootID,
         Content,
+        AuthorCommentID: self.authorID,
       }, function(res) {
         if(res.success) {
           self.ref.comment.element.scrollIntoView();
