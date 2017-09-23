f2640997d6ab7374334352297cf6090ed835221b
army8735 <army8735@qq.com>
2017-09-23 15:34:59 +0800

    edit

--- src/my/Profile.jsx
+++ src/my/Profile.jsx
@@ -12,13 +12,20 @@ class Profile extends migi.Component {
       // $(vd.element).find('.card').toggleClass('alt');
     }
   }
+  clickEdit(e) {
+    e.preventDefault();
+    let name = window.prompt('请输入想要修改的昵称', window.$CONFIG.userName).trim();
+    if(name !== window.$CONFIG.userName) {
+      alert(name);
+    }
+  }
   render() {
     return <div class="profile" onClick={ { '.card': this.click } }>
       <div class="bg"/>
       <div class="card">
         <img class="pic" src={ window.$CONFIG.userPic || '//zhuanquan.xyz/img/blank.png' }/>
         <div class="con">
-          <h3>{ window.$CONFIG.userName || '--' }<span>个人身份</span></h3>
+          <h3>{ window.$CONFIG.userName || '--' }<a href="#" class="edit" onClick={ this.clickEdit }>编辑</a></h3>
         </div>
       </div>
     </div>;
